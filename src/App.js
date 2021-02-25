a224fcb14986495a3ca80ad91e7625769cb3efac
Kasper Lind Sørensen <kasper.lind.sorensen@gmail.com>
2021-02-25 22:42:17 +0100

    Moved the styling of App into the js file.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,57 @@
 import logo from './logo.svg';
-import './App.css';
+import makeStyles from '@material-ui/core/styles/makeStyles'
+
+const useStyles = makeStyles(theme => ({
+  app: {
+    textAlign: 'center'
+  },
+  appLogo: {
+    height: '40vmin',
+    pointerEvents: 'none',
+    '@media (prefers-reduced-motion: no-preference)': {
+      animation: '$App-logo-spin infinite 20s linear'
+    }
+  },
+  appHeader: {
+    backgroundColor: '#282c34',
+    minHeight: '100vh',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    fontSize: 'calc(10px + 2vmin)',
+    color: 'white'
+  },
+  appLink: {
+    color: '#61dafb'
+  },
+  '@keyframes App-logo-spin': {
+    from: {
+      transform: 'rotate(0deg)'
+    },
+    to: {
+      transform: 'rotate(360deg)'
+    }
+  }
+}))
 
 function App() {
+  const {
+    app,
+    appLogo,
+    appHeader,
+    appLink
+  } = useStyles()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className={app}>
+      <header className={appHeader}>
+        <img src={logo} className={appLogo} alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className={appLink}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
